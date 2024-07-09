e3048afdc721c11a02fed02444297159d27592de
Daniele03 <324683@studenti.unimore.it>
2024-07-09 22:17:42 +0200

    Nuovo riassunto

---
+++ alberi_binari.cc
@@ -0,0 +1,18 @@
+/*
+ * Riassunto sulle principali primitive e funzione per quento rigurdano gli alberi binari (BST)
+ */
+
+#include<iostream>
+using namespace std;
+
+/********************************ALBERI BINARI ********************************/
+
+//Definizione della struttura nodo
+struct bnode {
+    int inf;
+    bnode* left;        // nodo sinistro
+    bnode* right;       // nodo destro
+    bnode* parent;      // collegamento al padre
+};
+//Dichiarazione del t324683ipo di dato binary tree
+typedef bnode* btree;
