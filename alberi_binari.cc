de745adc8bd027c8258c4504e79b100eea2fad62
Daniele03 <324683@studenti.unimore.it>
2024-07-09 22:45:36 +0200

    Aggiornamento riassunto

--- alberi_binari.cc
+++ alberi_binari.cc
@@ -5,7 +5,7 @@
 #include<iostream>
 using namespace std;
 
-/********************************ALBERI BINARI ********************************/
+/********************************ALBERI BINARI********************************/
 
 //Definizione della struttura nodo
 struct bnode {
@@ -16,3 +16,5 @@ struct bnode {
 };
 //Dichiarazione del t324683ipo di dato binary tree
 typedef bnode* btree;
+
+/****************************ALBERI BINARI DA RICERCA***************************/
